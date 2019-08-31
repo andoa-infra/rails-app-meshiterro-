7ab008d65b9fd0994e3943c569f8b718a3645b37
andoa-infra <ando.shoya.infratop@gmail.com>
2019-08-31 14:36:20 +0900

    [middle]meshiterro

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '78e5c9f74f8b42b235650d6e6ad8967ba7d2ad41aff879b324f6cce8b930f61a46c9f20cb8a0981737804244a652c0bee87f79039799020f0ecb69aecde682ee'
